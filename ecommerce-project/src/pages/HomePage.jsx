747985e8dee7fcfe20b6475ac41591263c22b6a0
ve00ym588 <snazkani@yamaha-motor-india.com>
2026-06-09 17:45:28 +0530

    create multiple pages

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import {Header} from "./Header"
 import logowhite from "../assets/logo-white.png";
 import mobilelogowhite from "../assets/mobile-logo-white.png";
 import searchicon from "../assets/search-icon.png";
@@ -10,37 +11,7 @@ import "./header.css"
 export function HomePage(){
     return (
         <>
-            <div className="header">
-        <div classNameName="left-section">
-            <a href="index.html" className="header-link">
-            <img className="logo"
-                src={logowhite}/>
-            <img className="mobile-logo"
-                src={mobilelogowhite} />
-            </a>
-        </div>
-
-        <div className="middle-section">
-            <input className="search-bar" type="text" placeholder="Search" />
-
-            <button className="search-button">
-            <img className="search-icon" src={searchicon} />
-            </button>
-        </div>
-
-        <div className="right-section">
-            <a className="orders-link header-link" href="orders.html">
-
-            <span className="orders-text">Orders</span>
-            </a>
-
-            <a className="cart-link header-link" href="checkout.html">
-            <img className="cart-icon" src={carticon} />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-            </a>
-        </div>
-        </div>
+            <Header />
 
         <div className="home-page">
         <div className="products-grid">
